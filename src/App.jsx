9d32da54459c5d16e2ef85629128efdeb17021ca
Sudhakar_sudha <sudhakarsudha8672@gmail.com>
2025-09-09 22:30:20 +0530

    initial project setup commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        React + Tailwind CSS 🚀
+      </h1>
+    </div>
+  )
+}
